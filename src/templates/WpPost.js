5a1f363e743be42460abb3d1db6a6e105e111966
hellomaryjo <majorx3music@gmail.com>
2021-11-07 16:27:54 -0600

    add seo data

--- src/templates/WpPost.js
+++ src/templates/WpPost.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import { graphql, Link as GatsbyLink } from 'gatsby';
 import { Container, Heading, Text, Link } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet';
 
 const WpPost = ({ data }) => {
   console.log(data);
   const { wpPost } = data;
 
   return (
-    <Container maxW='xl' centerContent>
-      <Heading>{wpPost.title}</Heading>
-      <Text>
-        <span dangerouslySetInnerHTML={{ __html: wpPost.content }}></span>
-      </Text>
-      <Link as={GatsbyLink} to='/'>Back to blog</Link>
-    </Container>
+    <>
+      <Helmet>
+        <meta name='description' content={wpPost?.seo?.metaDesc} />
+      </Helmet>
+      <Container maxW='xl' centerContent>
+        <Heading>{wpPost.title}</Heading>
+        <Text>
+          <span dangerouslySetInnerHTML={{ __html: wpPost.content }}></span>
+        </Text>
+        <Link as={GatsbyLink} to='/'>Back to blog</Link>
+      </Container>
+    </>
   )
 }
 
@@ -25,6 +31,9 @@ export const query = graphql`
       id
       title
       content
+      seo {
+        metaDesc
+      }
     }
   }
 `
